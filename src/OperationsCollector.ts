deae3db595c5677382e6254c36e70edef95398df
Georges H <prismosoft@gmail.com>
2025-09-30 17:21:36 -0400

    Add support for path-level parameters in operations

--- src/OperationsCollector.ts
+++ src/OperationsCollector.ts
@@ -1,38 +1,38 @@
-import {OpenAPIVisitor, OperationContext} from "./openapi/OpenAPIVisitor";
-import * as lodash from "lodash";
-import pino from "pino";
-import {OpenAPIV3} from "openapi-types";
-import {N8NINodeProperties} from "./n8n/SchemaToINodeProperties";
-import {IOperationParser} from "./OperationParser";
-import {OptionsByResourceMap} from "./n8n/OptionsByResourceMap";
-import {INodeProperties} from "n8n-workflow";
-import {replacePathVarsToParameter} from "./n8n/utils";
-import {IResourceParser} from "./ResourceParser";
+import * as lodash from 'lodash';
+import { INodeProperties } from 'n8n-workflow';
+import { OpenAPIV3 } from 'openapi-types';
+import pino from 'pino';
+import { OptionsByResourceMap } from './n8n/OptionsByResourceMap';
+import { N8NINodeProperties } from './n8n/SchemaToINodeProperties';
+import { replacePathVarsToParameter } from './n8n/utils';
+import { OpenAPIVisitor, OperationContext } from './openapi/OpenAPIVisitor';
+import { IOperationParser } from './OperationParser';
+import { IResourceParser } from './ResourceParser';
 
 export class BaseOperationsCollector implements OpenAPIVisitor {
-    public readonly _fields: INodeProperties[]
-    private optionsByResource: OptionsByResourceMap = new OptionsByResourceMap()
+    public readonly _fields: INodeProperties[];
+    private optionsByResource: OptionsByResourceMap = new OptionsByResourceMap();
     private n8nNodeProperties: N8NINodeProperties;
 
     // Log context
-    private bindings: any
+    private bindings: any;
 
     constructor(
         doc: any,
         protected operationParser: IOperationParser,
         protected resourceParser: IResourceParser,
-        protected logger: pino.Logger
+        protected logger: pino.Logger,
     ) {
-        this._fields = []
-        this.n8nNodeProperties = new N8NINodeProperties(doc)
+        this._fields = [];
+        this.n8nNodeProperties = new N8NINodeProperties(doc);
     }
 
     get operations(): INodeProperties[] {
         if (this.optionsByResource.size === 0) {
-            throw new Error('No operations found in OpenAPI document')
+            throw new Error('No operations found in OpenAPI document');
         }
 
-        const operations = []
+        const operations = [];
         for (const [resource, options] of this.optionsByResource) {
             const operation: INodeProperties = {
                 displayName: 'Operation',
@@ -49,11 +49,11 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
             };
             operations.push(operation);
         }
-        return operations
+        return operations;
     }
 
     get fields() {
-        return [...this._fields]
+        return [...this._fields];
     }
 
     visitOperation(operation: OpenAPIV3.OperationObject, context: OperationContext) {
@@ -61,33 +61,33 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
             operation: {
                 pattern: context.pattern,
                 method: context.method,
-                operationId: operation.operationId
-            }
-        }
-        this.bindings = bindings
+                operationId: operation.operationId,
+            },
+        };
+        this.bindings = bindings;
         try {
-            this._visitOperation(operation, context)
+            this._visitOperation(operation, context);
         } catch (error) {
             // @ts-ignore
-            const data = {...this.bindings, error: `${error}`}
+            const data = { ...this.bindings, error: `${error}` };
             // @ts-ignore
-            this.logger.warn(data, 'Failed to parse operation')
+            this.logger.warn(data, 'Failed to parse operation');
         }
     }
 
     _visitOperation(operation: OpenAPIV3.OperationObject, context: OperationContext) {
         if (this.operationParser.shouldSkip(operation, context)) {
-            this.logger.info(this.bindings, 'Skipping operation')
-            return
+            this.logger.info(this.bindings, 'Skipping operation');
+            return;
         }
-        const {option, fields: operationFields} = this.parseOperation(operation, context);
-        const resources = operation.tags!!.map((tag: string) => this.resourceParser.value({name: tag}))
+        const { option, fields: operationFields } = this.parseOperation(operation, context);
+        const resources = operation.tags!!.map((tag: string) => this.resourceParser.value({ name: tag }));
         for (const resourceName of resources) {
-            const fields = lodash.cloneDeep(operationFields)
+            const fields = lodash.cloneDeep(operationFields);
             const operationName = option.name;
-            this.addDisplayOption(fields, resourceName, operationName)
+            this.addDisplayOption(fields, resourceName, operationName);
             this.optionsByResource.add(resourceName, option);
-            this._fields.push(...fields)
+            this._fields.push(...fields);
         }
     }
 
@@ -96,24 +96,26 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
      */
     parseFields(operation: OpenAPIV3.OperationObject, context: OperationContext) {
         const fields = [];
-        const parameterFields = this.n8nNodeProperties.fromParameters(operation.parameters)
+        // Merge path parameters with operation parameters
+        const allParameters = [...(context.path.parameters || []), ...(operation.parameters || [])];
+        const parameterFields = this.n8nNodeProperties.fromParameters(allParameters);
         fields.push(...parameterFields);
 
         try {
-            const bodyFields = this.n8nNodeProperties.fromRequestBody(operation.requestBody)
+            const bodyFields = this.n8nNodeProperties.fromRequestBody(operation.requestBody);
             fields.push(...bodyFields);
         } catch (error) {
-            const data = {...this.bindings, error: `${error}`}
+            const data = { ...this.bindings, error: `${error}` };
             // @ts-ignore
-            this.logger.warn(data, 'Failed to parse request body')
-            const msg = "There's no body available for request, kindly use HTTP Request node to send body"
+            this.logger.warn(data, 'Failed to parse request body');
+            const msg = "There's no body available for request, kindly use HTTP Request node to send body";
             const notice: INodeProperties = {
                 displayName: `${context.method.toUpperCase()} ${context.pattern}<br/><br/>${msg}`,
                 name: 'operation',
                 type: 'notice',
                 default: '',
-            }
-            fields.push(notice)
+            };
+            fields.push(notice);
         }
         return fields;
     }
@@ -124,16 +126,16 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
                 resource: [resource],
                 operation: [operation],
             },
-        }
+        };
         fields.forEach((field) => {
-            field.displayOptions = displayOptions
-        })
+            field.displayOptions = displayOptions;
+        });
     }
 
     protected parseOperation(operation: OpenAPIV3.OperationObject, context: OperationContext) {
-        const method = context.method
+        const method = context.method;
         const uri = context.pattern;
-        const parser = this.operationParser
+        const parser = this.operationParser;
         const option = {
             name: parser.name(operation, context),
             value: parser.value(operation, context),
@@ -148,7 +150,6 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
         };
         const fields = this.parseFields(operation, context);
 
-
         return {
             option: option,
             fields: fields,
@@ -158,7 +159,7 @@ export class BaseOperationsCollector implements OpenAPIVisitor {
 
 export class OperationsCollector extends BaseOperationsCollector {
     protected parseOperation(operation: OpenAPIV3.OperationObject, context: OperationContext) {
-        const result = super.parseOperation(operation, context)
+        const result = super.parseOperation(operation, context);
         const notice: INodeProperties = {
             displayName: `${context.method.toUpperCase()} ${context.pattern}`,
             name: 'operation',
@@ -169,7 +170,6 @@ export class OperationsCollector extends BaseOperationsCollector {
             default: '',
         };
         result.fields.unshift(notice);
-        return result
+        return result;
     }
 }
-
